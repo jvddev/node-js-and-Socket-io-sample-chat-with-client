857ef9c9d20a72b12e4681863bbfd15aa63803d7
javadhagh476 <javadh476@gmail.com>
2019-06-25 15:30:32 +0430

    add expressValidator

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require('express');
 
 var app = express();
 
-
+app.use(bodyParser.urlencoded({ extended : false }));
+app.use(bodyParser.json({ type : 'application/json' }));
+app.use(expressValidator());
 app.use(express.static('public'));
 require('./routes')(app);
 require('./sevices/erorrHandler')(app);
